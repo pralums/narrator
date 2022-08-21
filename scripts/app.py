7f1156513dfe0ca3b5087559b2b2007e408387e3
Heston Hoffman <hestonhoffman@gmail.com>
2022-08-21 00:50:51 -0700

    Add database handling

--- scripts/app.py
+++ scripts/app.py
@@ -2,7 +2,7 @@ from dis import dis
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from pymongo import MongoClient
+from pymongo import MongoClient, ReturnDocument
 from pprint import pprint
 import re
 
@@ -12,56 +12,57 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 # MongoDB
 client = MongoClient()
 db = client.karma_db
-
 collection = db.karma_db
 
-#post = {"author": "Mike",
-#        "text": "My first blog post!",
-#        "tags": ["mongodb", "python", "pymongo"],
-#        "date": datetime.datetime.utcnow()
-#       }
-
-#posts = db.posts
-#post_id = posts.insert_one(post).inserted_id
-#print(db.list_collection_names())
-
-#pprint.pprint(posts.find_one())
-
-#print(post_id)
-
-# Listens to incoming messages that contain "hello"
-# To learn available listener arguments,
-# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
-#@app.message("hello")
-#def message_hello(message, say):
-#    # say() sends a message to the channel where the event was triggered
-#    say(f"Hey there <@{message['user']}>!")
-
+# Finds the display_name associated with a Slack user ID
 def find_display_name(user):
     slack_user = re.sub('>','',re.sub('<','',re.sub('@','',user.strip())))
     profile = app.client.users_profile_get(user=slack_user)
     display_name = profile['profile']['display_name']
     return display_name
 
+# Handle database updates
+def db_update(filter, i):
+    if collection.find_one(filter):
+        collection.find_one_and_update(
+            filter, 
+            {'$inc': {'karma': i}},
+            return_document=ReturnDocument.AFTER
+            )
+    else:
+        filter['karma'] = i
+        collection.insert_one(filter)
 
-@app.message("\+\+")
-def add_karma(message, say):
-    # print(message)
-    temp_array = message['text'].split('++')
-    items = []
+# Add/remove karma from db entries
+def karmic_repercussion(item, effect):
+    if effect == 'plus':
+        db_update({"protagonist": item }, 1)
+    else:
+        db_update({"protagonist": item }, -1)
 
-    for item in temp_array:
-        if not item == '':
+def find_karma(item):
+    db_entry = collection.find_one({"protagonist": item })
+
+@app.message(re.compile("(?:\S+)+(?:\s|)(?:\+\+|--)"))
+def manage_karma(context, say):
+    #for doc in collection.find({}):
+    #    print(doc)
+    for item in context['matches']:
+        if '++' in item:
             if '@' in item:
-                display_name = find_display_name(user=item)
-                items.append(display_name)
-            else: 
-                items.append(item.strip())
-    for item in items:
-        db_stuff
+                item = find_display_name(re.sub('--','',re.sub('\+\+','', item)))
+            item = re.sub('\+\+','',item).strip()
+            karmic_repercussion(item,'plus')
+            find_karma(item)
+        else:
+            if '@' in item:
+                item = find_display_name(re.sub('--','',re.sub('\+\+','', item)))
+            item = re.sub('--','',item).strip()    
+            karmic_repercussion(item,'minus')
+            find_karma(item)
+            #say(f'{karma_item} has lost karma')
 
-    say(f"I found the following items: {items}")
 
-# Start your app
+# Start app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
